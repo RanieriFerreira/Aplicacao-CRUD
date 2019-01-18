2d2575972126e71cb3e9957cc13be6a8fa3d91e4
ranieri.ferreira <ranieri.ferreira@RANIERIFE-PC>
2019-01-18 15:49:26 -0200

    Alteração dos tipos NUMERIC para INT e adição dos DROPTABLES para recriar as tabelas do banco

--- ScriptBanco.sql
+++ ScriptBanco.sql
@@ -1,23 +1,29 @@
+use RelogioDePonto
+GO
+DROP TABLE Pertence;
+DROP TABLE Horas;
+DROP TABLE Funcionarios;
+DROP TABLE Projetos;
 
 CREATE TABLE Projetos (
-                Id NUMERIC NOT NULL,
+                Id INT NOT NULL,
                 Nome VARCHAR NOT NULL,
                 Detalhe VARCHAR,
-                Ativo BOOLEAN,
+                Ativo INT,
                 CONSTRAINT Projetos_pk PRIMARY KEY (Id)
 );
 
 
 CREATE TABLE Funciorarios (
-                Cpf NUMERIC NOT NULL,
+                Cpf INT NOT NULL,
                 Nome VARCHAR NOT NULL,
                 CONSTRAINT Funciorarios_pk PRIMARY KEY (Cpf)
 );
 
 
 CREATE TABLE Horas (
-                CpfFuncionarioHoras NUMERIC NOT NULL,
-                IdProjetoHoras NUMERIC NOT NULL,
+                CpfFuncionarioHoras INT NOT NULL,
+                IdProjetoHoras INT NOT NULL,
                 Entrada DATE NOT NULL,
                 Saida DATE NOT NULL,
                 CONSTRAINT Horas_pk PRIMARY KEY (CpfFuncionarioHoras, IdProjetoHoras)
@@ -25,8 +31,8 @@ CREATE TABLE Horas (
 
 
 CREATE TABLE Pertence (
-                CpfFuncionarioPertence NUMERIC NOT NULL,
-                IdProjetoPertence NUMERIC NOT NULL,
+                CpfFuncionarioPertence INT NOT NULL,
+                IdProjetoPertence INT NOT NULL,
                 CONSTRAINT Pertence_pk PRIMARY KEY (CpfFuncionarioPertence, IdProjetoPertence)
 );
 
@@ -35,26 +41,22 @@ ALTER TABLE Pertence ADD CONSTRAINT Projetos_Pertence_fk
 FOREIGN KEY (IdProjetoPertence)
 REFERENCES Projetos (Id)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+ON UPDATE NO ACTION;
 
 ALTER TABLE Horas ADD CONSTRAINT Projetos_Horas_fk
 FOREIGN KEY (IdProjetoHoras)
 REFERENCES Projetos (Id)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+ON UPDATE NO ACTION;
 
 ALTER TABLE Pertence ADD CONSTRAINT Funciorarios_Pertence_fk
 FOREIGN KEY (CpfFuncionarioPertence)
 REFERENCES Funciorarios (Cpf)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+ON UPDATE NO ACTION;
 
 ALTER TABLE Horas ADD CONSTRAINT Funciorarios_Horas_fk
 FOREIGN KEY (CpfFuncionarioHoras)
 REFERENCES Funciorarios (Cpf)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+ON UPDATE NO ACTION;
