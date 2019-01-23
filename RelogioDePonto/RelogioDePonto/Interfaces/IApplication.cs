ebd50a48c970c319179bdd01e0897a4956f402a1
ranieri.ferreira <ranieri.ferreira@RANIERIFE-PC>
2019-01-23 14:34:02 -0200

    Criação da interface genérica para Application e criação da ApplicationProjeto

--- RelogioDePonto/RelogioDePonto/Interfaces/IApplication.cs
+++ RelogioDePonto/RelogioDePonto/Interfaces/IApplication.cs
@@ -5,7 +5,20 @@ using System.Threading.Tasks;
 
 namespace RelogioDePonto.Interfaces
 {
-    public interface IApplication<T> where T : class
+    public interface IApplication<T> where T: class
     {
+        // Grupo para buscar
+        T Get(int id);
+        IEnumerable<T> Get();
+        //IQueryable<T> Query(Expression<Func<T, bool>> filter);
+
+        // Grupo para adicionar
+        void Add(T entity);
+
+        // Grupo para salvar
+        void Save();
+
+        // Grupo para deletar
+        void Remove(T entity);
     }
 }
