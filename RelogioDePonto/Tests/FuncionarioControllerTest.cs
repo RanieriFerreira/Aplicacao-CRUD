84c80c4938d322f080d5cf2b3784c60c91c5e1a6
ranieri.ferreira <ranieri.ferreira@RANIERIFE-PC>
2019-01-23 16:38:31 -0200

    Correção de erros do ambiente de teste

---
+++ RelogioDePonto/Tests/FuncionarioControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using RelogioDePonto;
+using RelogioDePonto.Applications;
+using RelogioDePonto.Controllers;
+namespace Tests
+{
+    public class FuncionariosControllerTest
+    {
+        private FuncionariosController _controller;
+        private ApplicationFuncionario _application;
+
+        public FuncionariosControllerTest()
+        {
+            //_application = new ApplicationFuncionarioFake();
+            //_controller = new FuncionariosController(EmpresaContext context);
+        }
+    }
+}
---
+++ RelogioDePonto/Tests/FuncionarioRepositorioFake.cs
@@ -0,0 +1,16 @@
+﻿using RelogioDePonto;
+using RelogioDePonto.Interfaces;
+using RelogioDePonto.Modelos;
+using RelogioDePonto.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tes
+
+{
+    public class FuncionarioRepositorioFake
+    {
+    }
+}
