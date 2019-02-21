35b6ade4a27dbdca7bf98d871c52ba88374bbed0
ranieri.ferreira <ranieri.ferreira@RANIERIFE-PC>
2019-02-21 15:08:06 -0300

    6.11.UpgradeSass 6.11.2.Animação do menu lateral

--- frontEnd/src/app/Components/menu/menu.component.ts
+++ frontEnd/src/app/Components/menu/menu.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding  } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
+  // animations: [
+  //   trigger('openClose', [
+  //     state('open', style({
+  //       height: '100%',
+  //     })),
+  //     state('closed', style({
+  //       height: '0%',
+  //       display: 'none'
+  //     })),
+  //     transition('open => closed', [
+  //       animate('0.5s ease-out')
+  //     ]),
+  //     transition('closed => open', [
+  //       animate('0.5s ease-in')
+  //     ]),
+  //   ]),
+  // ],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
   selectedItem: number = 1;
+  isOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+ 
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }      from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { ListRelacoesComponent } from './Components/list-relacoes/list-relacoes.
     FormsModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
